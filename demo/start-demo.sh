3ad5cd068191c15941d11905e8c5b8ba64e69692
Matt Fisher <mfisher87@gmail.com>
2025-10-31 19:47:38 -0600

    chmod content dir before starting jlab

--- demo/start-demo.sh
+++ demo/start-demo.sh
@@ -4,4 +4,6 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export CONTENT_DIR="$SCRIPT_DIR/content"
 
+chmod a+rw ${CONTENT_DIR}
+
 docker compose --file "${SCRIPT_DIR}/compose.yml" up --detach
